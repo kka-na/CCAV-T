b28392471fac257822ace49d9cdfee9c562695e4
Kana Kim <69347961+kka-na@users.noreply.github.com>
2025-02-25 17:59:46 +0900

    Update avante.py

--- selfdriving/transmitter/avante.py
+++ selfdriving/transmitter/avante.py
@@ -80,19 +80,19 @@ class Avante():
     def setup_message_dicts(self):
 
         self.EAIT_Control_01 = {
-            'EPS_En': 0x00,  # 0x00: Disabled, 0x01: Enabled
-            'EPS_Override_Ignore': 0x00,  # 0x00: Disabled, 0x01: Enabled
-            'EPS_Speed': 10,  # 1.0 * (value) [0,250]
-            'ACC_En': 0x00,  # 0x00: Disabled, 0x01: Enabled
-            'AEB_En': 0x00,  # 0x00: Disabled, 0x01: Enabled
-            'Turn_Signal': 0x00,  # 0x00: Off, 0x01: Emergency, 0x02: Left, 0x03: Right
-            'AEB_decel_value': 0,  # 0.01 * (value) [0.0,1.0]
-            'Aliv_Cnt': 0  # 1.0 * (value) [0,255]
+            'EPS_En': 0x00, 
+            'EPS_Override_Ignore': 0x00,  
+            'EPS_Speed': 10,  
+            'ACC_En': 0x00,  
+            'AEB_En': 0x00,  
+            'Turn_Signal': 0x00,  
+            'AEB_decel_value': 0, 
+            'Aliv_Cnt': 0  
         }
 
         self.EAIT_Control_02 = {
-            'EPS_Cmd': 0,  # 0.1 * (value) [-500, 500] "deg"
-            'ACC_Cmd': 0  # 0.01 * (value + 10.23) [-3, 1] "m/s^2"
+            'EPS_Cmd': 0,  
+            'ACC_Cmd': 0 
         }
     
     def cleanup(self):
